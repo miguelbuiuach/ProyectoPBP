a76bc742c9e0c102c95a856fa5c0e2b03992d37c
Aketzalli Garcia <a299437@uach.mx>
2020-11-27 14:25:07 -0700

    JSON, libraries and layout

--- UsualApp/app/src/main/java/mx/uach/usualapp/MainActivity.kt
+++ UsualApp/app/src/main/java/mx/uach/usualapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package mx.uach.usualapp
 
+import android.app.DownloadManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://raw.githubusercontent.com/miguelbuiuach/ProyectoPBP/master/server/about/about.json"
+
+        val jsonRequest : StringRequest = StringRequest(Request.Method.GET, url, Response.Listener {  response->
+            Log.i("JSON", response.toString())
+        }, Response.ErrorListener {  error ->
+            Log.e("JSON", "Error en petición ${error.toString()}")
+        })
+
+        queue.add(jsonRequest)
     }
 }
