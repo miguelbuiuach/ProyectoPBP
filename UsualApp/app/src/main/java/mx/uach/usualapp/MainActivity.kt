897f907227a9483d4c3d48c40da6ae1e62c6d322
chamuko <a312730@uach.mx>
2020-11-30 00:16:19 -0700

    Fixed a reference for rvSuspectes in MainActivity

--- UsualApp/app/src/main/java/mx/uach/usualapp/MainActivity.kt
+++ UsualApp/app/src/main/java/mx/uach/usualapp/MainActivity.kt
@@ -3,6 +3,8 @@ package mx.uach.usualapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
 import com.android.volley.Response
@@ -10,16 +12,17 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import mx.uach.usualapp.Adapters.suspectAdapter
 import mx.uach.usualapp.models.suspect
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rvSuspects = findViewById<RecyclerView>(R.id.rvSuspects)
+        val sus = findViewById<RecyclerView>(R.id.rvSuspects)
 
         val queue = Volley.newRequestQueue(this)
-        val url = "https://pastebin.com/raw/sQY0Vrwz/"
+        val url = "https://pastebin.com/raw/nfAYJYfu"
 
 
         val jsonRequest : StringRequest = StringRequest(Request.Method.GET, url, Response.Listener { response->
@@ -28,7 +31,8 @@ class MainActivity : AppCompatActivity() {
             val itemType = object : TypeToken<List<suspect>>(){}.type
             val suspects : List<suspect> = gson.fromJson(response.toString(), itemType)
 
-            this.rvSuspects
+            findViewById<RecyclerView>(R.id.rvSuspects).adapter = suspectAdapter(suspects)
+            findViewById<RecyclerView>(R.id.rvSuspects).layoutManager = LinearLayoutManager(this)
 
             Log.i("SUSPECTS", suspects.toString())
 
